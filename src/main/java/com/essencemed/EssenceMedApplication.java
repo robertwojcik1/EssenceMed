e662963535c6931d641bc8115292a90bde860865
Robert Wójcik <robert.wojcik.programista@gmail.com>
2023-08-07 11:43:55 +0200

    basic project structure with dependencies

---
+++ src/main/java/com/essencemed/EssenceMedApplication.java
@@ -0,0 +1,13 @@
+package com.essencemed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EssenceMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EssenceMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/essencemed/EssenceMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essencemed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EssenceMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
